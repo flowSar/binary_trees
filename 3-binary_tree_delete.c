bfee17f3bed562ca5f64d829800cc003940dbd38
flowsar <brahim.sar151@gmail.com>
2024-06-05 00:40:39 +0100

    delete entire tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - delete the entire tree
+ *
+ * tree: root of the tree that we're suppose to delete
+ */
+void travel_and_delete(binary_tree_t *tree);
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	travel_and_delete(tree);
+}
+
+void travel_and_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	travel_and_delete(tree->left);
+	travel_and_delete(tree->right);
+	free(tree);
+}
