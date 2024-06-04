85e2e274a105467e3e47dc6312e6e4532b0bc020
flowsar <brahim.sar151@gmail.com>
2024-06-04 23:39:45 +0100

    create new node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_t - create new node in the binary tree
+ *
+ * parent: parent of the new node in the tree
+ * value: size of array
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+    
+    new_node = malloc(sizeof(binary_tree_s));
+    if (new_node == NULL)
+        return NULL;
+    new_node->n = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    new_node->parent = parent;
+    return  (new_node);
+}
+
