fa2d76b94446082029f0d6089aa0c8320b7c9e31
flowsar <brahim.sar151@gmail.com>
2024-06-05 22:33:06 +0100

    binary_tree_inorder

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - traverse the tree and print each node value
+ *
+ * @tree: root of the tree
+ * @func: function pointer for printing node value
+ */
+void traverse_tree_and_print(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		traverse_tree_and_print(tree, func);
+	}
+}
+/**
+ * traverse_tree_and_print - traverse the tree and print each node value
+ * by using recursive function
+ *
+ * @tree: root of the tree
+ * @func: pointer to function for printing node value
+ */
+void traverse_tree_and_print(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	traverse_tree_and_print(tree->left, func);
+	traverse_tree_and_print(tree->right, func);
+	func(tree->n);
+}
