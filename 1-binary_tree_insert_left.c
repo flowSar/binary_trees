733dcb461845e5ad4797608af485e3d8763706b9
flowsar <brahim.sar151@gmail.com>
2024-06-05 11:30:04 +0100

    up

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,7 +24,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		left_child_node = parent->left;
-
 		parent->left = new_node;
 		new_node->left = left_child_node;
         
