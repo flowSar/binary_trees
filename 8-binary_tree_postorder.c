529bdfbdbef630fcb3098537e7eb0cac7fb904a0
flowsar <brahim.sar151@gmail.com>
2024-06-05 22:51:51 +0100

    binary_tree_postorder

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 /**
- * binary_tree_inorder - traverse the tree and print each node value
+ * binary_tree_postorder - traverse the tree and print each node value
  *
  * @tree: root of the tree
  * @func: function pointer for printing node value
  */
 void traverse_tree_and_print(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree != NULL && func != NULL)
 	{
@@ -25,7 +25,6 @@ void traverse_tree_and_print(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL)
 		return;
 	traverse_tree_and_print(tree->left, func);
-	func(tree->n);
 	traverse_tree_and_print(tree->right, func);
-
+	func(tree->n);
 }
