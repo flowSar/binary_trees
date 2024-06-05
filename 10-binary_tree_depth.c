a3be9cc25b1e1812c142aafdb1070fa4cd48dd39
flowsar <brahim.sar151@gmail.com>
2024-06-06 00:12:46 +0100

    depth of the node in a tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - calculate the depth of the tree
+ *
+ * @tree: root of the tree
+ * Return: height of the tree
+ */
+int traverse_tree_backward(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	int n_node;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->parent == NULL)
+		return (0);
+	n_node = traverse_tree_backward(tree);
+	return (n_node);
+}
+/**
+ * traverse_tree_backward - traverse the tree backward
+ * to get the number of edge you will pass to reach the
+ * giving node
+ *
+ * @tree: root of the tree
+ * Return: return number of edge you traverse from the giving node
+ */
+int traverse_tree_backward(const binary_tree_t *tree)
+{
+	int edge;
+
+	if (tree == NULL)
+		return (-1);
+	edge = 1 + traverse_tree_backward(tree->parent);
+	return (edge);
+}
