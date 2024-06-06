cd0ef7ba83a03e8963ab0b8f551cdec5f90c1fa9
flowsar <brahim.sar151@gmail.com>
2024-06-06 12:00:36 +0100

    calculate number of leaves in a tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,12 +1,42 @@
 #include "binary_trees.h"
+size_t traverse_tree(const binary_tree_t *tree, const binary_tree_t *parent);
 /**
- * binary_tree_leaves - 
+ * binary_tree_leaves - calcultae number of leaves in the tree
  *
  * @tree: root of the tree
- * Return: 
+ * Return: number of leaves
  */
-
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	
+	size_t leaf_number;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	leaf_number = traverse_tree(tree, tree);
+	return (leaf_number);
+}
+/**
+ * traverse_tree - traverse the tree cand calculate leaf_number
+ * of leafs in the tree
+ *
+ * @tree: root of the tree
+ * @parent: root of the tree to keep
+ * Return: return size of tree that
+ */
+size_t traverse_tree(const binary_tree_t *tree, const binary_tree_t *parent)
+{
+	int leaf_nimber = 0;
+
+	if (tree == NULL)
+	{
+		if (parent->left == NULL && parent->right == NULL)
+			return (1);
+
+		return (-1);
+	}
+	leaf_nimber += traverse_tree(tree->left, tree);
+	leaf_nimber += traverse_tree(tree->right, tree);
+	return (leaf_nimber);
 }
