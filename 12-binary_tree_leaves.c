ac2c9b553208641599d8095674469c043c607675
flowsar <brahim.sar151@gmail.com>
2024-06-06 12:30:08 +0100

    fix

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -15,7 +15,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	leaf_number = traverse_tree(tree, tree);
-	return (leaf_number);
+	return ((leaf_number + 1) / 2);
 }
 /**
  * traverse_tree - traverse the tree cand calculate leaf_number
@@ -27,16 +27,16 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
  */
 size_t traverse_tree(const binary_tree_t *tree, const binary_tree_t *parent)
 {
-	int leaf_nimber = 0;
+	int leaf_number = 0;
 
 	if (tree == NULL)
 	{
 		if (parent->left == NULL && parent->right == NULL)
 			return (1);
 
-		return (-1);
+		return (0);
 	}
-	leaf_nimber += traverse_tree(tree->left, tree);
-	leaf_nimber += traverse_tree(tree->right, tree);
-	return (leaf_nimber);
+	leaf_number += traverse_tree(tree->left, tree);
+	leaf_number += traverse_tree(tree->right, tree);
+	return (leaf_number);
 }
