71a536c6fe2f30f719e5759adb90c0265b81caa2
flowsar <brahim.sar151@gmail.com>
2024-06-07 00:16:41 +0100

    number of node with one child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-size_t traverse_tree(const binary_tree_t *tree, const binary_tree_t *parent);
+int traverse_tree(const binary_tree_t *tree, int number);
 /**
  * binary_tree_nodes - calculate number of nodes with at least one child
  *
@@ -12,10 +12,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
-	node_number = traverse_tree(tree, tree);
-	if (node_number > 1 && (tree->left != NULL || tree->right != NULL))
-		return (node_number + 1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	node_number = traverse_tree(tree, 0);
 	return (node_number);
 }
 /**
@@ -23,20 +22,18 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
  * with a least one child except the root of the tree
  *
  * @tree: root of the tree
- * @parent: root of the tree to keep
+ * @number: number of node with at least 1 child
  * Return: return number of nodes with at least one child
  */
-size_t traverse_tree(const binary_tree_t *tree, const binary_tree_t *parent)
+int traverse_tree(const binary_tree_t *tree, int number)
 {
-	int node_number = 0;
+	int node_num = number;
 
 	if (tree == NULL)
-	{
-		if (parent->left != NULL || parent->right != NULL)
-			return (1);
-		return (0);
-	}
-	node_number += traverse_tree(tree->left, tree);
-	node_number += traverse_tree(tree->right, tree);
-	return (node_number);
+		return (node_num + 0);
+	if (tree->left != NULL || tree->right != NULL)
+		node_num += 1;
+	node_num = traverse_tree(tree->left, node_num);
+	node_num = traverse_tree(tree->right, node_num);
+	return (node_num);
 }
