04cd72805e39ea13f0a1f0f4e4e034ab29e7cb6d
flowsar <brahim.sar151@gmail.com>
2024-06-07 00:36:18 +0100

    fix balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,6 @@
 #include "binary_trees.h"
+int traverse_left_tree(const binary_tree_t *tree);
+int traverse_right_tree(const binary_tree_t *tree);
 /**
  * binary_tree_balance - calculate balance of power number of chldren
  * for each parent
@@ -11,12 +13,42 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->right == NULL && tree->left == NULL)
-		return (0);
-	else if (tree->left != NULL && tree->right != NULL)
-		return (2);
-	else if (tree->right == NULL || tree->left == NULL)
-		return (1);
+	int right = traverse_right_tree(tree);
+	int left = traverse_left_tree(tree);
 
-	return (0);
+	return (left - right);
+}
+/**
+ * traverse_left_tree - traverse the right of the tree
+ * and return height of the left side.
+ *
+ * @tree: root of the tree
+ * Return: height of the left side
+ */
+int traverse_left_tree(const binary_tree_t *tree)
+{
+	int node_num = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	node_num = traverse_left_tree(tree->left);
+	return (node_num + 1);
+}
+/**
+ * traverse_right_tree - traverse the right of the tree
+ * and return height of the right side.
+ *
+ * @tree: root of the tree
+ * Return: height of the right side
+ */
+int traverse_right_tree(const binary_tree_t *tree)
+{
+	int node_num = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	node_num = traverse_right_tree(tree->right);
+	return (node_num + 1);
 }
