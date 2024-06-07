4007f824ffd4e5b246197335d7477d0ee9bf2771
flowsar <brahim.sar151@gmail.com>
2024-06-07 23:35:55 +0100

    node with siblings

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,12 +1,19 @@
 #include "binary_trees.h"
 /**
- * binary_tree_sibling - 
+ * binary_tree_sibling - check the node has siblings
  *
- * @tree: root of the tree
- * Return: 
+ * @node: node
+ * Return: return node sibligns otherwise NULL
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+	else if (node == node->parent->right)
+		return (node->parent->left);
+	return (NULL);
 }
