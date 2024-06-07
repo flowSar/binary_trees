936dd411732e3cc459e88c0ae2c30a9688a2f04a
flowsar <brahim.sar151@gmail.com>
2024-06-07 22:06:39 +0100

    update the code

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
-int traverse_left_tree(const binary_tree_t *tree);
-int traverse_right_tree(const binary_tree_t *tree);
+
+int traverse_tree(const binary_tree_t *tree);
 /**
- * binary_tree_is_full - check if the tree id binary
+ * binary_tree_is_full - check if the tree is full
  *
  * @tree: root of the tree
  * Return: 1 of tree is full and 0 otherwise
@@ -10,59 +10,28 @@ int traverse_right_tree(const binary_tree_t *tree);
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int left;
-	int right;
+	int result;
 
-	if (tree == NULL)
+	result = traverse_tree(tree);
+	if (result % 2 == 0)
 		return (0);
-	left = traverse_left_tree(tree);
-	right = traverse_right_tree(tree);
-
-	if (left != right)
-		return (0);
-	else if (left == 0 && right == 0)
-		return (0);
-	else
-		return (1);
+	return (1);
 }
 /**
- * traverse_left_tree - traverse the right of the tree
- * and return height of the left side.
+ * traverse_tree - traverse the tree and calculate number of
+ * node is the tree
  *
  * @tree: root of the tree
- * Return: height of the left side
+ * Return: number of nodes in the tree.
  */
-int traverse_left_tree(const binary_tree_t *tree)
+int traverse_tree(const binary_tree_t *tree)
 {
-	int node_num;
+	int left = 0;
+	int right = 0;
 
 	if (tree == NULL)
-		return (1);
-	if (tree->left == NULL && tree->right != NULL)
 		return (0);
-	if (tree->left != NULL && tree->right == NULL)
-		return (0);
-	node_num = traverse_left_tree(tree->left);
-	return (node_num);
-}
-/**
- * traverse_right_tree - traverse the right of the tree
- * and return height of the right side.
- *
- * @tree: root of the tree
- * Return: height of the right side
- */
-int traverse_right_tree(const binary_tree_t *tree)
-{
-	int node_num;
-
-	if (tree == NULL)
-		return (1);
-	if (tree->left == NULL && tree->right != NULL)
-		return (0);
-	if (tree->left != NULL && tree->right == NULL)
-		return (0);
-
-	node_num = traverse_right_tree(tree->right);
-	return (node_num);
+	left = traverse_tree(tree->left);
+	right = traverse_tree(tree->right);
+	return (left + right + 1);
 }
